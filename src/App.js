b5b137bb0d38f704627ade2837f912870124084a
TheGoodHatZ <christophe.gonay17@gmail.com>
2023-11-26 18:07:06 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+
 import './App.css';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Menu from './components/Menu';
+import Footer from './components/Footer';
+
+
 
 function App() {
   return (
+    <>
+    <Nav/>
+    <Main/>
+    <Menu/>
+    <Footer/>
+    </>
  
-    <div>
-      <h1>Meta Frontend Developer Capstone Starter Files</h1>
-    </div>
+   
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,39 @@
+import React from "react";
+import logo from '../images/Logo .svg'
+
+const Footer = () => {
+    return (
+        <footer>
+            <section>
+                <div className="company-info">
+                    <img src={logo} alt=""/>
+                    <p>We are a family owned Mediterranean restaurent, focused on traditional recipes served with a modern twist.</p>
+                </div>
+                <div>
+                    <h3>Important Links</h3>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Menu</a></li>
+                        <li><a href="/">Reservations</a></li>
+                        <li><a href="/">Order Online</a></li>
+                        <li><a href="/">Login</a></li>
+
+
+
+
+                    </ul>
+                </div>
+
+                <div>
+                    <h3>Contact</h3>
+                    <ul>
+                        <li>Address :<br/> 123 Towncity, USA </li>
+                    </ul>
+                </div>
+            </section>
+        </footer>
+    );
+};
+
+export default Footer;
